6f1789d998ebb23c372e5a0370b573dc8142fcf3
Jessica Robles <j.roblesm@uniandes.edu.co>
2022-05-27 17:41:32 -0500

    base foro con stud

---
+++ Usuarios/answers/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from students.models import Student
+from questions.models import Question
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, default=None)
+    text = models.CharField(max_length=1000)
+    author = models.CharField(max_length=1000, default="")
+    dateTime = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s %s' % (self.question, self.text)
---
+++ Usuarios/students/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
+
+from . import views
+
+urlpatterns = [
+    path('students/', views.student_list, name='studentList'),
+    path('studentcreate/', csrf_exempt(views.student_create), name='studentCreate'),
+]
